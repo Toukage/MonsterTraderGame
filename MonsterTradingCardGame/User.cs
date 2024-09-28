531e6025f34db6cb58bc8ec8075a082603fa37cb
sura hameed <sura.hameed@gmx.at>
2024-09-28 21:16:56 +0200

    added User Class structure

---
+++ MonsterTradingCardGame/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Users
+{
+
+    public string Username 
+    { 
+        get; 
+        set; 
+    }
+    public string Password //needs to be hashed 
+    { 
+        get; 
+        set; 
+    }
+
+}
