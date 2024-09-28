8d0eeeae0b2348df3eae68055f8e766161a21a64
sura hameed <sura.hameed@gmx.at>
2024-09-28 17:53:33 +0200

    added database class

---
+++ MonsterTradingCardGame/Database.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+public class Class1
+{
+	public Class1()
+	{
+		string connectionString = "Host=localhost;Port=5432;Username=sura2011tele2@gmail.com;Password=Namjoon0912;Database=monster_trading_card_game";
+
+	}
+}
