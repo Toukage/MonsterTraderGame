cdcd7cefc8e61cabb34ffffeb457f4974ad0e32b
sura hameed <sura.hameed@gmx.at>
2024-10-04 17:43:26 +0200

    noch nicht called/implemented functionen wurden auskommentiert

--- MonsterTradingCardGame/User.cs
+++ MonsterTradingCardGame/User.cs
@@ -5,11 +5,13 @@ using System.Xml.Serialization;//library for lists
 public class User
 {
     //users info
+
     public string Username 
     { 
         get; 
         private set; 
     }
+
     public string Password //needs to be hashed 
     { 
         get; 
@@ -21,19 +23,21 @@ public class User
         get;
         private set;
     } = 20;//every user starts with 20 coins
+
     public int ELO
     {
         get;
         private set;
     } = 100; //starting score is 100
 
-
     //users cards
+
     public List<Card> Stack 
     { 
         get; 
         private set; 
     }
+
     public List<Card> Deck 
     { 
         get; 
@@ -48,8 +52,7 @@ public class User
         Deck = new List<Card>();
     }
 
-
-    private string HashPassword(string password) //implement Hashing logic
+    /*private string HashPassword(string password) //implement Hashing logic
     {
 
     }
@@ -58,19 +61,22 @@ public class User
     {
         Stack.Add(card);
     }
+
     public void RemoveStack(Card card)//removes card from stack
     {
         Stack.Remove(card);
     }
+
     public void AddDeck(Card card)//adds card to deck (implement max cards!!!)
     {
         Deck.Add(card);
     }
+
     public void RemoveDeck(Card card)//removes card from deck
     {
         Deck.Remove(card);
     }
 
     //add pack purchasing logic
-    //add elo system
+    //add elo system */
 }
