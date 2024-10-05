b8c8d55d7c0440443bf0a2da674343c7fad3e3f8
sura hameed <sura.hameed@gmx.at>
2024-10-05 13:04:56 +0200

    UserManagement class erstellt, Login method moved

---
+++ MonsterTradingCardGame/UserManagement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+
+public class UserManagement
+{
+	public UserManagement()
+	{
+       
+    }
+
+    public void Login(HttpListenerRequest request, HttpListenerResponse response)
+    {
+        response.StatusCode = 200; // code for "OK"
+        byte[] buffer = Encoding.UTF8.GetBytes("Login works LETS GO RIGBY");
+        response.OutputStream.Write(buffer, 0, buffer.Length);
+    }
+}   
