03fc25d637627f33597c2f87ca127263704db338
sura hameed <sura.hameed@gmx.at>
2024-10-06 17:48:24 +0200

    register function gelöscht, in UserManagement implented, datenbank verbindung mit NpgSqlConnection eingerichtet

--- MonsterTradingCardGame/Database.cs
+++ MonsterTradingCardGame/Database.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
  */
 public class Database
 {
-	private readonly string connectionString = "Host=localhost;Port=5432;Username=toukage;Password=mtcgserver;Database=MTCG_DB";
+	private static string connectionString = "Host=localhost;Port=5432;Username=toukage;Password=mtcgserver;Database=MTCG_DB";
 
-    public void Register(string userName, string passWord)
+	public static NpgsqlConnection Connection()
 	{
-
+        NpgsqlConnection connection = new NpgsqlConnection(connectionString);//erstellt eine verbindung zur datenbank aus den infos von connectionString
+		connection.Open(); // opend die connection zur datenbank
+		return connection;
 	}
+   
 	
 }
